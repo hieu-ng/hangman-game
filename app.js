49058d1d3e3bef166a144dc97fa62f4effe6cbed
hieu-ng <hieutnguyen06@gmail.com>
2020-05-25 09:21:43 +1000

    Show notification if wrong

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ function displayWord() {
 
 }
 
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
 window.addEventListener('keydown', e => {
   // console.log(e.keyCode);
   if (e.keyCode >= 65 && e.keyCode <= 90) {
