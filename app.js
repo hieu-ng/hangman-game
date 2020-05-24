2c7147ecfefdf0263cfa97cfa36c733e70138d08
hieu-ng <hieutnguyen06@gmail.com>
2020-05-25 09:18:36 +1000

    Listening to what is being typed

--- app.js
+++ app.js
@@ -33,4 +33,29 @@ function displayWord() {
 
 }
 
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWord();
