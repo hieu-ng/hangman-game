e25d3e763cb29e413a665a199741c68e798e0d0b
hieu-ng <hieutnguyen06@gmail.com>
2020-05-25 09:06:13 +1000

    Get random word from words array

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words.[Math.floor(Math.random() * word.length)];
